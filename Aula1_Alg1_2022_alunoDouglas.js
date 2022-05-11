ea4ae12ff24faebd2c97fcdf84c6e9ed276eb81e
Douglas-Lorenzi-Saldanha <douglaslorenzisaldanha@gmail.com>
2022-05-10 23:36:11 -0300

    Add files via upload

--- Aula1_Alg1_2022_alunoDouglas.js
+++ Aula1_Alg1_2022_alunoDouglas.js
@@ -8,3 +8,19 @@ nome = prompt('Digite o nome: ')
 sobrenome = prompt('Digite o sobrenome:')
 
 alert(nome + ' ' + sobrenome)
+
+alert('')
+
+//Desafio Aula 1
+remetente = prompt('Digite o Remetente: ')
+destinatario = prompt('Digite o Destinatário: ')
+mensagem = prompt('Mensagem para enviar: ')
+
+alert('')
+
+//Mensagem
+
+alert('Remetente: ' + remetente)
+alert('Destinatário: ' + destinatario)
+alert('')
+alert(mensagem)
