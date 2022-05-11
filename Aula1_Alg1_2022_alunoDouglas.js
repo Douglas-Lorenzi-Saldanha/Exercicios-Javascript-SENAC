78d69dcd06fb56cce04b478433fe51cb63048b1d
Douglas-Lorenzi-Saldanha <douglaslorenzisaldanha@gmail.com>
2022-05-10 23:23:07 -0300

    Add files via upload

--- Aula1_Alg1_2022_alunoDouglas.js
+++ Aula1_Alg1_2022_alunoDouglas.js
@@ -2,3 +2,9 @@
 
 /*alert('Meu primeiro programa!') nesse caso funciona quando roda dentro do navegador*/
 console.log("Também da para fazer assim!")
+
+//entrada de dados
+nome = prompt('Digite o nome: ')
+//alert(nome)
+console.log(nome)
+
