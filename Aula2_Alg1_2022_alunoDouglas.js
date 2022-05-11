eb7736a349e8fa0b1663973b0dfeb3a79e5bad91
Douglas-Lorenzi-Saldanha <douglaslorenzisaldanha@gmail.com>
2022-05-11 00:19:03 -0300

    Add files via upload

--- Aula2_Alg1_2022_alunoDouglas.js
+++ Aula2_Alg1_2022_alunoDouglas.js
@@ -3,4 +3,21 @@ var numeroB = prompt('Insira o numero B: ')
 
 var resultado = parseInt(numeroA) + parseInt(numeroB)
 
-alert('Resultado: ' + resultado)
+alert('Resultado da soma: ' + resultado)
+
+resultado = parseInt(numeroA) - parseInt(numeroB)
+
+alert('Resultado da subtracao: ' + resultado)
+
+resultado = parseInt(numeroA) * parseInt(numeroB)
+
+alert('Resultado da multiplicacao: ' + resultado)
+
+resultado = parseInt(numeroA) / parseInt(numeroB)
+
+alert('Resultado da divisao: ' + resultado)
+
+resultado = 2 * (parseInt(numeroA) + parseInt(numeroB))
+
+alert('Resultado do dobro do resultado da Soma: ' + resultado)
+
