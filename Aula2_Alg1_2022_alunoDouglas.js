663ec1141f5de65f2887f5047766f9dea384f942
Douglas-Lorenzi-Saldanha <douglaslorenzisaldanha@gmail.com>
2022-05-11 00:15:16 -0300

    Add files via upload

---
+++ Aula2_Alg1_2022_alunoDouglas.js
@@ -0,0 +1,6 @@
+var numeroA = prompt('Insisra o numero A: ')
+var numeroB = prompt('Insira o numero B: ')
+
+var resultado = parseInt(numeroA) + parseInt(numeroB)
+
+alert('Resultado: ' + resultado)
